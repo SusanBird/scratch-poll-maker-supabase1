5037449c1c78bd00043cb4f139426794dfa30d54
SusanBird <susanbird391@gmail.com>
2022-03-25 11:53:11 -0700

    signUp function

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3eXZsb2V0c2h2ZWZra3J5d3prIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc4Nzk0MzEsImV4cCI6MTk2MzQ1NTQzMX0.8lVyqbhvuR5E2KD1n187Bk0teEnC6OCfd7h2Kkj_Oew';
 
 export async function getUser() {
@@ -18,7 +19,7 @@ export async function getPolls() {
         .from('polls')
         .select();
 
-    return response.data;
+    return response.body;
 }
 
 export async function createPoll(question, option1, option2, votes1, votes2) {
@@ -34,5 +35,18 @@ export async function createPoll(question, option1, option2, votes1, votes2) {
             },
         ]);
 
-    return response.data;
+    return response.body;
+}
+
+export async function signUp(realEmail, realPassword) {
+    console.log('before sign up', client.auth.user());
+
+    const response = await client.auth.signUp({
+        email: realEmail,
+        password: realPassword,
+    });
+
+    console.log('after sign up', client.auth.user());
+
+    return response.user;
 }
